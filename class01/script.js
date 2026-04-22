cca45e0c39beabaf39cb96468e728b939a51916d
Mary Grace Nazareno <mgracenazareno@gmail.com>
2026-04-22 14:25:37 -0400

    add class1 notes & mini-exercise

---
+++ class01/script.js
@@ -0,0 +1,113 @@
+console.debug("Hello World");
+
+// variables
+//let gives us the possibility to change a value
+let number = 10;
+number = number + 1;
+console.log(number);
+
+// const means the variable should not change
+const courseName = "Web Interface Programming";
+// courseName = "Mary" ; constant cannot be change
+console.log(courseName);
+
+let numbers = [1, 2, 3];
+console.log(numbers);
+
+// in this case, an array is assigned to const grades
+// we cannot change the variable name grades itself
+// but we can manipulate the array
+const grades = [80, 90, 100];
+grades.push(75); // we're not changing the variable, just using the array
+console.log(grades);
+
+// JavaScript Object
+const student = {
+  name: "Lii",
+  grade: 87,
+};
+
+console.log(student);
+console.log(student.name);
+console.log(student.grade);
+
+/**Block Scope
+ * Scope protects variables and prevents accidental misuse.
+ */
+a = 5;
+
+if (a == 5) {
+  let message = "You guessed the number right!!!";
+  console.log(message);
+}
+
+// WARNING: this wouldn't work, because message is
+// in the conditional's scropt and not accessible
+// console.log(message)
+
+function showUser() {
+  const username = "Mina";
+  console.log(`${username} is logged in!`);
+}
+
+showUser();
+
+// WARNING: this wouldn't work, because message is
+// in the conditional's scropt and not accessible
+// console.log(username);
+
+/**
+ * Mini check in
+ */
+
+// let or const
+page = 1; //page can change
+maxItems = 20; //  maxItems could not change!
+categories = ["movies", "books"]; //categories stay the same
+
+/**
+ * Functions
+ */
+
+function greetUser(name) {
+  // this is a void function
+  console.log("hello " + name + " i hope you're good");
+}
+
+//const name = "Nora";
+//greetUser(name);
+
+// const result = add(3, 5);
+// console.log("result is " + result);
+
+// a void function, execute whatever it's supposed to
+// but it doesn't return any values
+// hence, it can't be assigned to a variable
+
+// let a = greetUser() doesn't work, since greetUser doesn't return anything
+
+// BUT, a return function can be assigned to a variable
+// Because, it returns a value!
+// let number1 = add(10, 20);
+
+// Mini Exercise :
+const name = "Jane";
+const lastName = "Doe";
+
+const fullName = getFullName(name, lastName);
+// 1.
+// write a RETURN function that adds name and
+// lastname (with a space) and returns it to fullName
+function getFullName(name, lastname) {
+  return `${name} ${lastname}`;
+}
+
+// 2.
+// write a function that greets the user calling their fullname
+function greetings() {
+  console.log(fullName);
+}
+
+console.log(getFullName(name, lastName));
+// console.log(greetings());  //undefined
+greetings();
