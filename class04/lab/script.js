49447d570d24fbc019c20f1cdcd885922bafa3ea
Mary Grace Nazareno <mgracenazareno@gmail.com>
2026-04-30 14:42:14 -0400

    added dom references

---
+++ class04/lab/script.js
@@ -0,0 +1,7 @@
+// DOM References
+const studentBtn = document.getElementById("load-student-btn");
+const courseBtn = document.getElementById("load-courses-btn");
+const clearBtn = document.getElementById("clear-btn");
+const status = document.getElementById("status");
+const studentData = document.getElementById("student-container");
+const courseData = document.getElementById("courses-container");
