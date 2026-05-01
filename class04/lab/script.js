7635803ba9a4cf886b0b1587a34a5e2b9b3e644c
Mary Grace Nazareno <mgracenazareno@gmail.com>
2026-05-01 06:06:39 -0400

    implement student loading with Promise

--- class04/lab/script.js
+++ class04/lab/script.js
@@ -1,26 +1,10 @@
-const student = [
-  {
-    id: 1,
-    name: "Mary Brown",
-    program: "Web Design",
-    semester: "Winter 2026",
-    bio: "Web Developer",
-  },
-  {
-    id: 2,
-    name: "James Bond",
-    program: "Cybersecurity",
-    semester: "Winter 2026",
-    bio: "Cybersecurity Specialist",
-  },
-  {
-    id: 3,
-    name: "Tom Jones",
-    program: "Web Design",
-    semester: "Winter 2026",
-    bio: "Frontend Developer",
-  },
-];
+const student = {
+  id: 1,
+  name: "Mary Brown",
+  program: "Web Design",
+  semester: "Winter 2026",
+  bio: "Web Developer",
+};
 
 const courses = [
   { code: "WIP2", title: "Web Interface Programming 2" },
@@ -32,27 +16,42 @@ const courses = [
 const studentBtn = document.getElementById("load-student-btn");
 const courseBtn = document.getElementById("load-courses-btn");
 const clearBtn = document.getElementById("clear-btn");
-const status = document.getElementById("status");
+const statusText = document.getElementById("status");
 const studentData = document.getElementById("student-container");
 const courseData = document.getElementById("courses-container");
 
-console.log(student);
 function getStudentData() {
   let promise = new Promise((resolve, reject) => {
-    if (auth == true) {
-      resolve(student);
-    } else {
-      reject("Authentication failed");
-    }
+    setTimeout(() => {
+      if (student) {
+        resolve(student);
+      } else {
+        reject("No students registered!");
+      }
+    }, 1000);
   });
   return promise;
 }
 
-//testing
-getStudentData()
-  .then((student) => {
-    console.log(student);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+function renderStudent(student) {
+  studentData.innerHTML = `
+  <h3>Student Profile</h3>
+  <p><strong>Name: </strong>${student.name}</p>
+  <p><strong>Program: </strong> ${student.program}</p>
+  <p><strong>Semester: </strong>${student.semester}</p>
+  <p><strong>Bio: </strong> ${student.bio}</p>`;
+}
+
+studentBtn.addEventListener("click", () => {
+  console.log("Button Clicked!");
+  statusText.textContent = "Loading Students...";
+
+  getStudentData()
+    .then((student) => {
+      renderStudent(student);
+      statusText.textContent = "Student loaded successfully!";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
